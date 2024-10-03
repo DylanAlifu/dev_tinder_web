ee6e9ab10fd328b499597aecdef3c14b9422c60f
DylanAlifu <dylann5326@gmail.com>
2024-10-03 11:45:39 -0700

    feat: connection request

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -1,5 +1,69 @@
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addRequest } from "../utils/requestSlice";
+
 const Requests = () => {
-  return <div>Requests</div>;
+  const dispatch = useDispatch();
+  const requests = useSelector((store) => store.request);
+
+  const fetchRequest = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/user/requests/pending", {
+        withCredentials: true,
+      });
+      dispatch(addRequest(res?.data?.data));
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchRequest();
+  }, []);
+
+  if (!requests || requests.length === 0) {
+    return (
+      <div className="flex justify-center my-10">
+        <h1 className="font-bold text-2xl">No Requests Found!</h1>
+      </div>
+    );
+  }
+
+  return (
+    <div className="text-center">
+      <h1 className="font-bold text-2xl my-10">Requests</h1>
+
+      {requests.map((request) => {
+        const { fromUserId } = request;
+
+        return (
+          <>
+            <div key={fromUserId._id} className="flex justify-center">
+              <div className="m-2 md:m-10 p-4 rounded-lg bg-base-300 w-[75%] md:w-[50%] flex items-center justify-around gap-8">
+                <div className="flex flex-col items-center">
+                  <img
+                    src={fromUserId.url}
+                    alt={`${fromUserId.firstName} ${fromUserId.lastName}`}
+                    className="w-20 h-20 object-cover rounded-full"
+                  />
+                  <h2 className="font-bold text-xl w-fit">
+                    {fromUserId.firstName} {fromUserId.lastName}
+                  </h2>
+                </div>
+                <div className="flex flex-col gap-4">
+                  <button className="btn btn-error btn-sm">Accept</button>
+                  <button className="btn btn-primary btn-sm">Reject</button>
+                </div>
+              </div>
+            </div>
+          </>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Requests;
